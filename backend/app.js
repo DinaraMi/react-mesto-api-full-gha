7a5a3620ed1ead162b7c1bc0286d5aade0360d63
Dinara M <d9268843743@yandex.ru>
2023-09-27 10:20:14 +0200

    Проектная работа 15.17 фронт и бэк

--- backend/app.js
+++ backend/app.js
@@ -11,16 +11,13 @@ const { handleErrors } = require('./middlewares/handleErrors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-app.use(limiter);
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   'https://photo.space.nomoredomainsrocks.ru',
 ];
-
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -41,16 +38,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(helmet());
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   family: 4,
 });
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
 
 app.use(requestLogger);
 
