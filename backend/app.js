6351960f32925f9bc60422fa37f81fd5c24e46ba
Dinara M <d9268843743@yandex.ru>
2023-09-27 09:20:55 +0200

    Проектная работа 15.15 фронт и бэк

--- backend/app.js
+++ backend/app.js
@@ -2,31 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cors = require('cors');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet');
+// const rateLimit = require('express-rate-limit');
 const indexRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleErrors } = require('./middlewares/handleErrors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-const corsOptions = {
-  origin: 'https://photo.space.nomoredomainsrocks.ru',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
-
-app.use(helmet());
+// app.use(helmet());
 app.use(express.json());
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+// });
+// app.use(limiter);
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
